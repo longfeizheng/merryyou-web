fed8543cfaacf01dc02f1fb001112fb331cfd933
longfeizheng <longfei_zheng@163.com>
2018-05-23 21:05:52 +0800

    实现vue Oauth2登录

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,11 @@
     <el-col :xs="24" :sm="18" :md="14" :lg="6" id="main">
       <el-input v-model="info.username" placeholder="请输入用户名"></el-input>
       <el-input v-model="info.password" placeholder="请输入密码"></el-input>
-      <el-button  class="btn-auto" @click="login" type="success">登录</el-button>
+      <el-row>
+        <el-button  class="btn-auto" @click="login" type="success">密码模式</el-button>
+        <el-button  class="btn-auto" @click="customLogin" type="success">自定义登录</el-button>
+        <!--<label>{{token}}</label>-->
+      </el-row>
     </el-col>
     <template>
       <router-link to="newcontact">{{msg}}</router-link>
@@ -17,6 +21,7 @@ export default {
   data () {
     return {
       msg: 'Welcome',
+      // token: '',
       info: {
         username: '',
         password: ''
@@ -26,18 +31,42 @@ export default {
   methods: {
     login () {
       this.$axios({
-        methods: 'POST',
-        url: '/api/login',
-        data: {
-          username: 'admin',
-          password: '123456'
+        method: 'post',
+        url: '/api/oauth/token',
+        params: {
+          'username': this.info.username,
+          'password': this.info.password,
+          'grant_type': 'password',
+          'scope': 'all'
+        },
+        auth: {
+          username: 'merryyou',
+          password: 'merryyou'
         },
         headers: {
-          'Authorization': 'Basic bWVycnl5b3U6bWVycnl5b3U=',
           'Content-Type': 'application/x-www-form-urlencoded'
         }
       }).then(function (res) {
         console.log(res)
+      })
+    },
+    customLogin () {
+      this.$axios({
+        method: 'post',
+        url: '/api/login',
+        params: {
+          'username': this.info.username,
+          'password': this.info.password
+        },
+        auth: {
+          username: 'merryyou',
+          password: 'merryyou'
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (res) {
+        console.log(res.data.access_token)
       })
     }
   }
@@ -67,5 +96,6 @@ a {
 .btn-auto {
   width: 100%;
   margin-top: 12px;
+  margin-left: 0px;
 }
 </style>
