24f2e9c42bd81514f06d9c631d7664cd873cbd01
longfeizheng <longfei_zheng@163.com>
2018-05-23 10:06:18 +0800

    增加localStorage存储

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
+    <img src="./assets/logo.png">
+    <br />
     <router-view/>
   </div>
 </template>
--- src/components/NewContact.vue
+++ src/components/NewContact.vue
@@ -1,29 +1,32 @@
 <template>
   <el-row>
-    姓名：{{info.name}}
-    <el-input v-model="info.name" placeholder="请输入姓名"></el-input>
-    年龄：{{info.age}}
-    <el-input v-model="info.age" placeholder="请输入年龄"></el-input>
-    性别：{{info.sex}}
-    <el-select v-model="info.sex" placeholder="请选择">
-      <el-option v-for="item in options" :key="item" :value="item"></el-option><!-- 这里的key官方推荐在v-for时使用，不然会警告，但不影响使用 -->
-    </el-select>
-    <el-button @click="create">创建</el-button>
-    <template>
-      <el-table :data="tabledata" align="left">
-        <el-table-column prop="name" label="姓名"></el-table-column>
-        <el-table-column prop="age" label="年龄"></el-table-column>
-        <el-table-column prop="sex" label="性别"></el-table-column>
-        <el-table-column label="操作">
-          <template slot-scope="a">
-            <el-button size="mini" type="danger" @click="del(a.$index)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </template>
+    <el-col :xs="24" :sm="18" :md="14" :lg="10" id="main">
+      <label>姓名：</label>
+      <el-input v-model="info.name" placeholder="请输入姓名"></el-input>
+      <label>年龄：</label>
+      <el-input v-model="info.age" placeholder="请输入年龄"></el-input>
+      <label>性别：</label>
+      <el-select v-model="info.sex" placeholder="请选择">
+        <el-option v-for="item in options" :key="item" :value="item"></el-option><!-- 这里的key官方推荐在v-for时使用，不然会警告，但不影响使用 -->
+      </el-select>
+      <el-button  class="btn-auto" @click="create" type="success">创建</el-button>
+      <template>
+        <el-table :data="tabledata" align="left">
+          <el-table-column prop="name" label="姓名"></el-table-column>
+          <el-table-column prop="age" label="年龄"></el-table-column>
+          <el-table-column prop="sex" label="性别"></el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="a">
+              <el-button size="mini" type="danger" @click="del(a.$index)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </template>
+    </el-col>
   </el-row>
 </template>
 <script>
+import Storage from '../store/store'
 export default {
   name: 'NewContact',
   data () {
@@ -36,11 +39,7 @@ export default {
       options: [
         '女', '男', '保密'
       ],
-      tabledata: [
-        {name: 'Leo', age: 12, sex: 'man'},
-        {name: 'Lei', age: 22, sex: 'women'},
-        {name: 'Li', age: 15, sex: 'man'}
-      ]
+      tabledata: Storage.fetch()
     }
   },
   methods: {
@@ -51,9 +50,30 @@ export default {
     del (index) {
       this.tabledata.splice(index, 1)
     }
+  },
+  watch: { // 新添加，watch是vue的监听，一旦监听对象有变化就会执行相应操作
+    tabledata: { // 新添加，被监听的对象
+      handler (items) {
+        Storage.save(items) //  新添加，监听对象变化后所做的操作，一个函数，保存数据
+      },
+      deep: true // 深度监听，避免数据的嵌套层数太多，我们要使用深度监听，这样即使数据层级过多监听不到
+    }
   }
 }
 </script>
 <style>
-
+  #main{
+    float: none;
+    margin: 0 auto;
+  }
+  .el-input{
+    padding-bottom: 5px;
+  }
+  .el-select {
+    display: block;
+  }
+  .btn-auto{
+    width: 100%;
+    margin-top: 12px;
+  }
 </style>
